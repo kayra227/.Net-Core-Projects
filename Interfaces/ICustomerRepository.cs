bc01ac7a12ba77c5cbab4f5efa63525ffd995cda
kayra227 <120187593+kayra227@users.noreply.github.com>
2024-12-14 18:18:52 +0300

    Add files via upload

---
+++ Interfaces/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using AspNetCoreMvc_MovieSales.Models;
+using AspNetCoreMvc_MovieSales.ViewModels;
+
+namespace AspNetCoreMvc_MovieSales.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        public List<Customer> GetAll();
+        public Customer GetById(int id);
+        public void Add(Customer customer);
+        public void Update(Customer customer);
+        public void Delete(Customer customer);
+        void Update(CustomerViewModel customerViewModel);
+    }
+}
